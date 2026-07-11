b5b0729061bd68aebc9bd3f4cb874ade3ca39f35
Mo7a-95 <kleel.su@gmail.com>
2026-07-11 12:30:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(
         
-    " مرحباً بك،... 👋 \n\n"
+    "مرحباً بك،... 👋 \n\n"
     "أنا مساعدك الذكي والذي يعمل بالذكاء الإصطناعي .\n\n"
     "✨ يمكنني مساعدتك في :\n\n"
     "💬 الإجابة على الأسئلة\n"
